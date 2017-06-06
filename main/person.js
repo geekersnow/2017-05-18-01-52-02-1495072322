6fa98e2587ac0fd4019358cd17647df4e3fbc877
geekersnow <kaishiaini1314@126.com>
2017-06-06 13:12:16 +0800

    Update person.js

--- main/person.js
+++ main/person.js
@@ -4,6 +4,6 @@ function Person(name,age){
 	this.name = name; 
 	this.age = age; 
 	this.introduce = function(){
-		console.log('My name is '+this.name'. I am '+this.age+' years old.');
+		return "My name is "+this.name". I am "+this.age+" years old.";
 	}
 } 
